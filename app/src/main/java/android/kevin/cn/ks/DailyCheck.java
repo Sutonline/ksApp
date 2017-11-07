43f239c50d7693888a61374aab616f4b2b7861d2
yongkang.zhang <yongkang.zhang@7fresh.com>
2017-11-07 16:16:55 +0800

    初步集成boommenu

--- app/src/main/java/android/kevin/cn/ks/DailyCheck.java
+++ app/src/main/java/android/kevin/cn/ks/DailyCheck.java
@@ -2,9 +2,10 @@ package android.kevin.cn.ks;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 
-import com.nightonke.boommenu.BoomButtons.BoomButton;
+import com.nightonke.boommenu.BoomButtons.SimpleCircleButton;
+import com.nightonke.boommenu.BoomButtons.TextInsideCircleButton;
+import com.nightonke.boommenu.BoomMenuButton;
 
 /**
  * author: yongkang.zhang
@@ -13,10 +14,28 @@ import com.nightonke.boommenu.BoomButtons.BoomButton;
 
 public class DailyCheck extends Activity {
 
+    private static final int[] MENU_IMAGE = new int[]{R.drawable.bat, R.drawable.bear, R.drawable.butterfly, R.drawable.cat,
+        R.drawable.dolphin, R.drawable.horse, R.drawable.elephant, R.drawable.owl, R.drawable.squirrel
+    };
+
+    private static final String[] MENU_NAME = new String[]{"打卡", "熊", "蝴蝶", "猫", "海豚", "horse", "大象",
+        "猫头鹰", "松鼠"
+    };
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        BoomButton bmb = findViewById(R.id.boom_menu);
-        bmb.set
+        setContentView(R.layout.daily_check_layout);
+        initMenu();
+
+    }
+
+    private void initMenu() {
+        BoomMenuButton bmb = findViewById(R.id.boom_menu_button);
+        for (int i = 0; i < bmb.getPiecePlaceEnum().pieceNumber(); i++) {
+            TextInsideCircleButton.Builder builder = new TextInsideCircleButton.Builder().normalImageRes(MENU_IMAGE[i])
+                    .normalText(MENU_NAME[i]);
+            bmb.addBuilder(builder);
+        }
     }
 }
